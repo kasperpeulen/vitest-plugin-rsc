6edfc67831a64128a9e00c2913dc6b86f7af4d0e
Kasper Peulen <kasperpeulen@gmail.com>
2025-08-04 18:56:41 +0200

    Demo branch

--- playground/nextjs-notes-demo/components/note-editor.test.tsx
+++ playground/nextjs-notes-demo/components/note-editor.test.tsx
@@ -8,6 +8,11 @@ import { expectNavigation } from '../test/utilts'
 import { NextRouter } from './next-router'
 import NoteEditor from './note-editor'
 
+// Spy on the session module to create a mocked user
+vi.mock(import('../libs/session'), { spy: true })
+// Mock out the notes API that depends on @vercel/kv
+vi.mock(import('../libs/notes'), () => ({ getNote: vi.fn(), setNote: vi.fn() }))
+
 test('note editor saves note and redirects after submitting note', async () => {
   const created_by = 'kasper'
   vi.mocked(getUser).mockReturnValue(created_by)
